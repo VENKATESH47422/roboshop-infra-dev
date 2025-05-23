d538983b30876dfc47af8c5b6f3dd6a5c33d5dfe
venkatesh <venkateshvenky626@gmail.com>
2025-05-23 13:39:50 +0530

    first commit

--- 04-databases/bootstrap.sh
+++ 04-databases/bootstrap.sh
@@ -13,4 +13,4 @@ environment=$2 #dont use env here, it is reserved in linux
 #yum install python3.11-devel python3.11-pip -y
 yum install -y ansible python3-dnf
 pip3 install ansible botocore boto3
-ansible-pull -U https://github.com/Venkatesh7422/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
+ansible-pull -U https://github.com/VENKATESH47422/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
